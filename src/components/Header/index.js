cf4082856a6b14a966ebcf5667a9cdae3d4b236a
danilo <danilod11@live.com>
2019-03-21 19:32:54 -0300

    add css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 
+import './style.css'
 
 export default class Header extends Component {
   render() {
     return (
       <div>
-        <div>
+        <div className='header'>
             <Link to="/">Notas</Link>
             <Link to="/about">Sobre</Link>
         </div>
